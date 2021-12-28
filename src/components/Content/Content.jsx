1e7f8977c088012993010159b09cced14305e6eb
vladward <vladward13@gmail.com>
2021-12-28 18:36:39 +0300

    refactor Content component with props

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,48 +1,35 @@
 import React from 'react'
+import {useState} from 'react'
 import s from './Content.module.css'
 import gitImg from './../../common/assets/gitImg.png'
 import burger from './../../common/assets/burger.png'
 import counter from './../../common/assets/counter.png'
 import SN from './../../common/assets/SN.png'
 import todo from './../../common/assets/Todolist.png'
+import currency from './../../common/assets/currency.png'
 
 
-export const Content = () => {
+export const Content = (props) => {    
     return (
         <div className={s.content}>
             <div className={s.headerText}>
-                <h1>Vladisalv Malohvey</h1>
-                <h3>Front-end developer</h3>
-                <p>its layout. The point of using Lorem Ipsum is that it has a more-or-less normal
-                    distribution of letters, as opposed to using 'Content here, content here', making
-                    it look like readable English. </p>
+                <h1>{props.aboutMe.name}</h1>
+                <h3>{props.aboutMe.profession}</h3>
+                <p>{props.aboutMe.description}</p>
             </div>
 
-            <div className={s.workExpirience}>
+            <div className={s.workExperience}>
                 <h2>Work expirience</h2>
-                <li>
-                    System administrator
-                    Health care institution “Shchuchin Central District Hospital” (2014 - 2016)
-                </li>
-                <li>
-                    Driver
-                    Logistics transport company (2016 - 2021)
-                </li>
-                <li>
-                    Junior React developer (2021 - present)
-                </li>
+                {props.workExp && props.workExp.map(w => {
+                    return <li key={w.id}>{w.title}</li>
+                })}
             </div>
 
             <div className={s.education}>
                 <h2>Education</h2>
-                <li>
-                    Information technology software
-                    Technological College Educational Institution "The Yanka Kupala State University of Grodno" (2010 -
-                    2014)
-                </li>
-                <li>
-                    Front-end React developer - It-incubator.by (2021/09 - 2022/02)
-                </li>
+                {props.education && props.education.map(e => {
+                    return <li key={e.id}>{e.title}</li>
+                })}
             </div>
 
             <div className={s.githubInfo}>
@@ -50,31 +37,17 @@ export const Content = () => {
                 <img src={gitImg} alt=""/>
             </div>
 
-            <div className={s.projects}>
-                <div className={s.projectBlock}>
-                    <div className={s.projectItem}>
-                        <h5>Burgers site with HTML, CSS and JS</h5>
-                        <a href="https://vladward.github.io/burger-vlad/">
-                            <img className={s.burger} src={burger} alt="burger"/>
-                        </a>
-                    </div>
-                    {/*<div className={s.projectItem}>*/}
-                    {/*    <h3>Social network with React, Typescript, Redux</h3>*/}
-                    {/*    <img src={SN} alt="sn"/>*/}
-                    {/*</div>*/}
-                    <div className={s.projectItem}>
-                        <h5>Todolist with React, Typescript, Redux and MUI</h5>
-                        <a href="https://vladward.github.io/todo/">
-                            <img className={s.todo} src={todo} alt="todo"/>
-                        </a>
-                    </div>
-                    <div className={s.projectItem}>
-                        <h5>Counter application with React, Typescript, Redux</h5>
-                        <a href="https://vladward.github.io/counter/">
-                            <img className={s.counter} src={counter} alt="counter"/>
-                        </a>
-                    </div>
-                </div>
+            <div className={s.projectItem}>
+                <h2>Projects links</h2>
+                {props.projects && props.projects.map(p => {
+                    return <li key={p.id}>
+                        <a href={p.link}>{p.title + ' ' + '-' + ' '}</a>
+                        {p.description}
+                        {p.wip
+                            ? <i style={{color: "gray"}}>(...wip)</i>
+                            : ''}
+                    </li>
+                })}
             </div>
         </div>
     )
