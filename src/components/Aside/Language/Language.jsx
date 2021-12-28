0e9ee14b92cc4d6b8f6adfe5c1170b9daaf25b45
vladward <vladward13@gmail.com>
2021-12-28 18:34:17 +0300

    add Language component

---
+++ src/components/Aside/Language/Language.jsx
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import s from './../Aside.module.css'
+
+export const Language = (props) => {
+    const [onOff, setOnOff] = useState(false)
+
+    const onFunction = () => {
+        setOnOff(!onOff)
+        props.setLang(true)
+    }
+
+    const offFunction = () => {
+        setOnOff(!onOff)
+        props.setLang(false)
+    }
+
+    const styleRu = {
+        width: "30px",
+        height: "30px",
+        border: "1px solid black",
+        borderRight: "none",
+        display: "inline-block",
+        backgroundColor: onOff ? "burlywood" : "white",
+        cursor: "pointer",
+        textAlign: "center",
+    }
+    const styleEng = {
+        width: "30px",
+        height: "30px",
+        border: "1px solid black",
+        borderLeft: "none",
+        display: "inline-block",
+        backgroundColor: onOff ? "white" : "burlywood",
+        cursor: "pointer",
+        textAlign: "center",
+    }
+    return (
+        <div className={s.radioContainer}>
+            <div style={styleRu} onClick={onFunction}>Ru</div>
+            <div style={styleEng} onClick={offFunction}>Eng</div>
+        </div>
+    )
+}
