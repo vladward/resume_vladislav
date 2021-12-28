82cddd0f14aa8aa5df9f031fc99c34f92b99d9f0
vladward <vladward13@gmail.com>
2021-12-28 18:59:54 +0300

    create Projects component

---
+++ src/components/Content/Projects/Projects.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import s from './../Content.module.css'
+
+
+export const Projects = (props) => {
+    return (
+        <div className={s.projectItem}>
+            <h2>Projects links</h2>
+            {props.projects && props.projects.map(p => {
+                return <li key={p.id}>
+                    <a href={p.link}>{p.title + ' ' + '-' + ' '}</a>
+                    {p.description}
+                    {p.wip
+                        ? <i style={{color: "gray"}}>(...wip)</i>
+                        : ''}
+                </li>
+            })}
+        </div>
+    )
+}
