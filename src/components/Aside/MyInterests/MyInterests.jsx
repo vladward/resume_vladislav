cda72170ee16e691ea002fb5438ce627a7ab739f
vladward <vladward13@gmail.com>
2021-12-28 18:33:58 +0300

    add MyInterests component

---
+++ src/components/Aside/MyInterests/MyInterests.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import s from './../Aside.module.css'
+
+export const MyInterests = (props) => {
+    return (
+        <div className={s.interests}>
+            <h2>{!props.changeLang ? "Interests:" : "Интересы"}</h2>
+            {props.interests.map(i => <p key={i.id}>{!props.changeLang ? i.title : i.titleRu}</p>)}
+        </div>
+    )
+}
