54d7a0536ded307813c208392721065bc2cc8e40
vladward <vladward13@gmail.com>
2021-12-29 17:27:46 +0300

    delete icons

--- src/components/Aside/MyContacts/MyContacts.jsx
+++ src/components/Aside/MyContacts/MyContacts.jsx
@@ -9,31 +9,23 @@ import github from './../../../common/assets/github.png'
 export const MyContacts = (props) => {
     return (
         <div className={s.contactItem}>
-            <h2>Contacts</h2>
-            <div className={s.contactIcon}>
-                <img src={mail} alt={props.contacts.mail}/>
-            </div>
+            <h2>{props.contactsTitle}</h2>
+
             <div className={s.contactText}>
                 <p>{props.contacts.mail}</p>
             </div>
 
-            <div className={s.contactIcon}>
-                <img src={location} alt={props.contacts.location}/>
-            </div>
+
             <div className={s.contactText}>
                 <p>{props.contacts.location}</p>
             </div>
 
-            <div className={s.contactIcon}>
-                <img src={linkedin} alt={props.contacts.linkedIn}/>
-            </div>
+
             <div className={s.contactText}>
                 <a href="https://www.linkedin.com/in/vlad-malohvey-129350219/">{props.contacts.linkedIn}</a>
             </div>
 
-            <div className={s.contactIcon}>
-                <img src={github} alt={props.contacts.github}/>
-            </div>
+
             <div className={s.contactText}>
                 <a href="https://github.com/vladward">{props.contacts.github}</a>
             </div>
