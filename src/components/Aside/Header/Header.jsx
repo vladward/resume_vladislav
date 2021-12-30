bd68dde3269dbfb5b175fb9809fde62fc20701a5
vladward <vladward13@gmail.com>
2021-12-30 13:24:13 +0300

    change name and profession display on different screens

--- src/components/Aside/Header/Header.jsx
+++ src/components/Aside/Header/Header.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import s from './../Aside.module.css'
 import ava1 from './../../../common/assets/ava2.png'
 
-export const Header = () => {
+export const Header = (props) => {
     return (
         <div className={s.header}>
             <img className={s.headerImg} src={ava1} alt="avatar"/>
+            <div className={s.headerAboutMe}>
+                <h1>{props.aboutMe.name}</h1>
+                <h5>{props.aboutMe.profession}</h5>
+            </div>
         </div>
     )
 }
--- src/components/Content/HeaderText/HeaderText.jsx
+++ src/components/Content/HeaderText/HeaderText.jsx
@@ -4,8 +4,10 @@ import s from './../Content.module.css'
 export const HeaderText = (props) => {
     return (
         <div className={s.headerText}>
-            <h1>{props.aboutMe.name}</h1>
-            <h3>{props.aboutMe.profession}</h3>
+            <div className={s.headerTextAboutMe}>
+                <h1>{props.aboutMe.name}</h1>
+                <h3>{props.aboutMe.profession}</h3>
+            </div>
             <p>{props.aboutMe.description}</p>
         </div>
     )
