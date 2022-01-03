10fae3fede6223a7549ca82f94965a57b1d340d9
vladward <vladward13@gmail.com>
2022-01-03 18:58:15 +0300

    ref: remove lang component

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -4,13 +4,11 @@ import {MyContacts} from './MyContacts/MyContacts'
 import {MySkills} from './MySkills/MySkills'
 import {MyInterests} from './MyInterests/MyInterests'
 import {Header} from './Header/Header'
-import {Language} from './Language/Language'
 
 export const Aside = (props) => {
     return (
         <div className={s.wrapper}>
             <Header aboutMe={props.aboutMe}/>
-            <Language setLang={props.setLang}/>
             <MySkills workWith={props.workWith} workWithTitle={props.workWithTitle}/>
             <MyInterests interests={props.interests} interestsTitle={props.interestsTitle}/>
             <MyContacts contacts={props.contacts} contactsTitle={props.contactsTitle}/>
